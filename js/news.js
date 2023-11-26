f24f0afe746e4604fd6495e8e3b7f519bd5c081d
tanukitrpg <149665808+tanukitrpg@users.noreply.github.com>
2023-11-26 14:44:35 +0800

    Update news.js

--- js/news.js
+++ js/news.js
@@ -1,4 +1,6 @@
 const blogPosts = [
+    
+    { title: "“跑团”，没有剧本的剧本杀？", link: "http://www.xinhuanet.com/globe/2023-05/11/c_1310715931.htm" },
     { title: "厌倦剧本杀后，“跑团”成为周末游戏新宠", link: "https://www.gcores.com/articles/163026" },
     { title: "众筹、铸币，“跑团”套住00后", link: "https://36kr.com/p/1842385107657990" },
     { title: "知乎关于跑团商业化模式镜土TRPG的看法，被围攻还是破局？", link: "https://www.zhihu.com/question/457165844" },
