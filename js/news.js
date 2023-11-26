b94a431f442998b9c033117528a6ce56eb709c2b
tanukitrpg <149665808+tanukitrpg@users.noreply.github.com>
2023-11-26 14:43:48 +0800

    Create news.js

---
+++ js/news.js
@@ -0,0 +1,5 @@
+const blogPosts = [
+    { title: "厌倦剧本杀后，“跑团”成为周末游戏新宠", link: "https://www.gcores.com/articles/163026" },
+    { title: "众筹、铸币，“跑团”套住00后", link: "https://36kr.com/p/1842385107657990" },
+    { title: "知乎关于跑团商业化模式镜土TRPG的看法，被围攻还是破局？", link: "https://www.zhihu.com/question/457165844" },
+];
