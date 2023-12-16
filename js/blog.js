e691cd0a02e258f9c323db54155cf9d178a1ffa1
tanukitrpg <149665808+tanukitrpg@users.noreply.github.com>
2023-12-16 18:11:18 +0800

    Update blog.js

--- js/blog.js
+++ js/blog.js
@@ -1,5 +1,6 @@
 const blogPosts = [
     { title: "文章导读：桌面角色扮演游戏（跑团）作为一种提高成人社会联系的治疗干预方法", link: "https://mp.weixin.qq.com/s?__biz=MzI5MDc4NzQ5MQ==&mid=2247483702&idx=1&sn=6577ed964f06b8770f814143c65cb4a6&chksm=ec1bd8fadb6c51ec6655972a57ab92b8c6ec42da9326de6f30a221e565bbe1ccb8396aa4d662&token=1663823005&lang=zh_CN#rd" },
     { title: "文章导读：将角色扮演游戏（跑团）用作青少年和成人的教育和治疗工具", link: "https://mp.weixin.qq.com/s?__biz=MzI5MDc4NzQ5MQ==&mid=2247483695&idx=1&sn=4a54330a60f361f2d51ca64056706dc6&chksm=ec1bd8e3db6c51f5e2c3167706ef579c642d22476d4adcb6e75c2f61f8edbfbdc6c878d0c820&token=1663823005&lang=zh_CN#rd" },
+    { title: "跑团电影与戏剧疗法", link: "https://mp.weixin.qq.com/s/qBEwAiD4geYOFbLNawG0Dg" },
     // 添加更多文章...
 ];
