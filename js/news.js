170925f7a5066dceef624bbc7f519e08bc7809d0
tanukitrpg <149665808+tanukitrpg@users.noreply.github.com>
2023-12-20 00:34:38 +0800

    Update news.js

--- js/news.js
+++ js/news.js
@@ -3,4 +3,6 @@ const blogPosts = [
     { title: "“跑团”，没有剧本的剧本杀？", link: "http://www.xinhuanet.com/globe/2023-05/11/c_1310715931.htm" },
     { title: "厌倦剧本杀后，“跑团”成为周末游戏新宠", link: "https://www.gcores.com/articles/163026" },
     { title: "众筹、铸币，“跑团”套住00后", link: "https://36kr.com/p/1842385107657990" },
+    { title: "剧本娱乐行业发展报告：2023年一季度剧本娱乐市场规模环比增长47%", link: "https://www.stcn.com/article/detail/872663.html" },
+    
 ];
