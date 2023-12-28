50b82bdc44266af8946dded7811569750b42ac5c
tanukitrpg <149665808+tanukitrpg@users.noreply.github.com>
2023-12-28 20:28:49 +0800

    Update news.js

--- js/news.js
+++ js/news.js
@@ -1,5 +1,5 @@
 const blogPosts = [
-    
+    { title: "厌倦剧本杀后，“跑团”成为周末游戏新宠", link: "https://mp.weixin.qq.com/s/PCzr2WOWNwgHPsI0Z-UN5g" },
     { title: "“跑团”，没有剧本的剧本杀？", link: "http://www.xinhuanet.com/globe/2023-05/11/c_1310715931.htm" },
     { title: "厌倦剧本杀后，“跑团”成为周末游戏新宠", link: "https://www.gcores.com/articles/163026" },
     { title: "众筹、铸币，“跑团”套住00后", link: "https://36kr.com/p/1842385107657990" },
