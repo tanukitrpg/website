999e2b9263a4c4e491bd8a6406be1990cd68335f
tanukitrpg <149665808+tanukitrpg@users.noreply.github.com>
2024-01-24 09:54:15 +0800

    Update news.js

--- js/news.js
+++ js/news.js
@@ -1,8 +1,8 @@
 const blogPosts = [
+    { title: "克学史话：克苏鲁神话在中国", link: "https://www.gcores.com/articles/142467" },
     { title: "年轻人的第一次跑团，不一定非要在《博德之门3》", link: "https://www.sohu.com/a/714948107_628730" },
     { title: "“跑团”，没有剧本的剧本杀？", link: "http://www.xinhuanet.com/globe/2023-05/11/c_1310715931.htm" },
     { title: "厌倦剧本杀后，“跑团”成为周末游戏新宠", link: "https://www.gcores.com/articles/163026" },
     { title: "众筹、铸币，“跑团”套住00后", link: "https://36kr.com/p/1842385107657990" },
     { title: "剧本娱乐行业发展报告：2023年一季度剧本娱乐市场规模环比增长47%", link: "https://www.stcn.com/article/detail/872663.html" },
-    
 ];
